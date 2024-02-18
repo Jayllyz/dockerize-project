b8f9da1716d6d261130cda2fc1670d0ecec766ff
Antony David <antonydavid945@gmail.com>
2024-02-18 21:15:11 +0100

    fix: catalog env process (#16)

--- tv-shows-catalog/client/src/routes/tv-shows/[id=integer]/[[slug]]/+page.server.ts
+++ tv-shows-catalog/client/src/routes/tv-shows/[id=integer]/[[slug]]/+page.server.ts
@@ -1,10 +1,11 @@
-import { API_BASE_URL } from '$env/static/private';
+//import { API_BASE_URL } from '$env/static/private';
 import type { TvShow } from '$lib/types/tv-show.interface';
 import { error, type NumericRange } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch, params: { id } }) => {
-	const response = await fetch(`${API_BASE_URL}/tv-shows/${id}`);
+	const url = process.env.API_BASE_URL;
+	const response = await fetch(`${url}/tv-shows/${id}`);
 	if (!response.ok) {
 		throw error(response.status as NumericRange<400, 599>, await response.json());
 	}
