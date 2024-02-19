f19fc52d12e8720cec442a48280aa6b143cc42df
MetooSHEESH <93775692+MetooSHEESH@users.noreply.github.com>
2024-02-19 09:55:05 +0100

    feat: added prod part for entrypoint

--- restaurant-website/entrypoint.sh
+++ restaurant-website/entrypoint.sh
@@ -5,3 +5,9 @@ if [ "${APP_ENV}" = "dev" ]; then
     chown -R "${USER}":"${USER}" /var/www/html
     symfony server:start --no-tls
 fi
+
+if [ "${APP_ENV}" = "prod" ]; then
+    php bin/console asset-map:compile
+    php bin/console cache:clear --env=prod
+    php-fpm
+fi
