3506a892a0487cc8e0acf1002b2ccc31c1e625e2
Antony David <antonydavid945@gmail.com>
2024-02-20 10:40:40 +0100

    refactor: restaurant composer setup (#25)

--- restaurant-website/entrypoint.sh
+++ restaurant-website/entrypoint.sh
@@ -2,14 +2,13 @@
 set -e
 
 if [ "${APP_ENV}" = "dev" ]; then
-    php bin/console importmap:install
+    composer install --no-interaction
     symfony server:start --no-tls
 fi
 
 if [ "${APP_ENV}" = "prod" ]; then
-    php bin/console importmap:install
+    composer install --no-interaction --no-dev --optimize-autoloader
     php bin/console asset-map:compile
     php bin/console cache:clear --env=prod
     php-fpm
 fi
-
