7f94746fc37aed5ca491fd371f7fa78960da55cb
Antony David <antonydavid945@gmail.com>
2024-02-24 13:01:23 +0100

    fix: health checks and perms (#29)

--- tv-shows-catalog/server/src/main.ts
+++ tv-shows-catalog/server/src/main.ts
@@ -16,7 +16,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  
+  await app.listen(port);
 }
 bootstrap().catch(console.error);
